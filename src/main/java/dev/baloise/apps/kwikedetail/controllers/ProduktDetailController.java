f6e57f3fb5ef19fca176f73599c1af31131d34f3
mateusz <mateuszmilart@gmail.com>
2020-09-21 14:29:20 +0200

    Refactor Static Assets

--- src/main/java/dev/baloise/apps/kwikedetail/controllers/ProduktDetailController.java
+++ src/main/java/dev/baloise/apps/kwikedetail/controllers/ProduktDetailController.java
@@ -2,13 +2,10 @@ package dev.baloise.apps.kwikedetail.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProduktDetailController {
-
 
     @RequestMapping("/")
     public String getProduktDetail(Model model) {

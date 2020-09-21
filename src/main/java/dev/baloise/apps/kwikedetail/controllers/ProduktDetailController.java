a3cece02ba0d5d5e182d0825e3b33488b8b46a0f
Peter Kullmann <peter.kullmann@me.com>
2020-09-21 14:37:10 +0200

    Show products

--- src/main/java/dev/baloise/apps/kwikedetail/controllers/ProduktDetailController.java
+++ src/main/java/dev/baloise/apps/kwikedetail/controllers/ProduktDetailController.java
@@ -2,14 +2,15 @@ package dev.baloise.apps.kwikedetail.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProduktDetailController {
 
-    @RequestMapping("/")
-    public String getProduktDetail(Model model) {
-        model.addAttribute("documentModel");
+    @RequestMapping("/{productId}")
+    public String getProduktDetail(Model model, @PathVariable("productId") String productId) {
+        model.addAttribute("productId", productId);
         return "product-detail";
     }
 }

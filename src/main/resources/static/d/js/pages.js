e070348f02307124c28c22865bd68d508d7392f2
Peter Kullmann <peter.kullmann@me.com>
2020-09-23 10:54:45 +0200

    Use shadow dom for details page

--- src/main/resources/static/d/js/pages.js
+++ src/main/resources/static/d/js/pages.js
@@ -1,5 +1,7 @@
 class DPages extends HTMLElement {
     connectedCallback() {
+        this.attachShadow({mode: "open"});
+
         this.render(window.location);
         this.unlisten = window.appHistory.listen(location =>
             this.render(location)
@@ -16,7 +18,7 @@ class DPages extends HTMLElement {
         if (match && match[1]) {
             fetch("/d/product/" + match[1])
                 .then(resp => resp.text())
-                .then(text => this.innerHTML = text);
+                .then(text => this.shadowRoot.innerHTML = text);
         }
     }
 

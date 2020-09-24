cfc52cb9656bed9169dba929c6e7e4e2e13ab4b1
Mateusz Milart <mateusz.milart@baloise.ch>
2020-09-24 18:16:29 +0200

    Add Shopping Cart Scrits, Recomendations Scripts and allow CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 
@@ -11,6 +11,9 @@ import { AppComponent } from './app.component';
     BrowserModule, HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
